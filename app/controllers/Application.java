add416cf71389100d8ab45c9554bde98affe7560
Ahmed Aswani <ahmed107@gmail.com>
2012-01-31 20:48:15 +0200

    first commit of ezay

---
+++ app/controllers/Application.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.data.validation.Required;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    public static void sayHello(@Required String myName) {
+    	 if(validation.hasErrors()) {
+    	        flash.error("Oops, please enter your name!");
+    	        index();
+    	    }
+    	render(myName);
+	}
+
+}
